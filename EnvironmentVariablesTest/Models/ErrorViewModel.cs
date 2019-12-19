174169d6f15a225a43686d669a46b5e542fc8526
Declan <dbaldwin@nla.co.uk>
2019-12-19 11:32:23 +0000

    Initial commit

---
+++ EnvironmentVariablesTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EnvironmentVariablesTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
